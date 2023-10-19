6bf533798ca6152fe37546704d1c93f9c64febdc
rohan <rd@slavepianos.org>
2023-10-20 02:13:27 +1100

    ~ts/repl,trackdeno

--- ts/repl.ts
+++ ts/repl.ts
@@ -1,11 +1,24 @@
-import { readLines } from "https://deno.land/std/io/buffer.ts";
+import { TextLineStream } from "https://deno.land/std/streams/text_line_stream.ts";
 
 import * as evaluate from './evaluate.ts'
 
+const stdinStream: ReadableStream = Deno.stdin.readable;
+
+const lineStream: ReadableStream = stdinStream.pipeThrough(new TextDecoderStream()).pipeThrough(new TextLineStream());
+
+const lineReader: ReadableStreamDefaultReader = lineStream.getReader();
+
 export async function interact<T>(processLine: (line: string) => T): Promise<void> {
-	for await (const line of readLines(Deno.stdin)) {
-		processLine(line);
-	}
+    return lineReader.read().then(({ done, value }) => {
+        if(done) {
+			// console.debug('repl: end of input');
+            return;
+        } {
+			// console.debug('repl: next line', value);
+			processLine(value);
+			return interact(processLine);
+		}
+	});
 }
 
 // Read and evaluate input per line.  Empty lines are not evaluated.
